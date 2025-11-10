657085c3c8155d0a6f14780edf983efb4f0d53f7
babykittenz <kidbymichaelb@gmail.com>
2025-11-10 12:21:09 -0600

    fix: update authenticator to follow session class

--- dynamic-web-applications/Core/Session.php
+++ dynamic-web-applications/Core/Session.php
@@ -34,4 +34,14 @@ class Session
         $_SESSION = [];
     }
 
+    public static function destroy(): void
+    {
+        static::flush();
+
+        session_destroy();
+
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly'] );
+    }
+
 }
